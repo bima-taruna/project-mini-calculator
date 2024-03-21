52c9eac09fea1961cd97c5d5cd319234dc4c8386
bima-taruna <bimataruna21@gmail.com>
2024-03-21 14:35:53 +0700

    refactor

--- script.js
+++ script.js
@@ -1,84 +1,89 @@
-const digits = document.querySelectorAll(".digit")
-const display = document.querySelector(".display")
-const zero = document.querySelector(".zero")
-const resetButton = document.querySelector('.clear')
-const addButton = document.querySelector(".add")
-const resultButton = document.querySelector(".result")
+const digits = document.querySelectorAll(".digit");
+const display = document.querySelector(".display");
+const zero = document.querySelector(".zero");
+const resetButton = document.querySelector(".clear");
+const addButton = document.querySelector(".add");
+const resultButton = document.querySelector(".result");
 let firstVal = null;
 let secondVal = null;
 let resultVal = null;
 let displayArr = [];
 
-buttonToDisplay()
-buttonZeroToDisplay()
-addTrigger()
-reset()
-triggerResult()
+digits.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (displayArr.length < 9) {
+      if (!item.classList.contains("operation")) {
+        assignFirst(item.textContent);
+      } else {
+        assignSecond(item.textContent);
+      }
+    }
+  });
+});
 
-function buttonToDisplay() {
-    digits.forEach((item) => {
-        item.addEventListener("click", () => {
-            if (displayArr.length < 9) {
-                if (!item.classList.contains("operation")) {
-                    displayArr.push(item.textContent)
-                    firstVal = parseInt(displayArr.join(""))
-                    secondVal = firstVal
-                    display.textContent = firstVal
-                } else {
-                    displayArr = []
-                    displayArr.push(item.textContent)
-                    secondVal = parseInt(displayArr.join(""))
-                    display.textContent = secondVal
-                }
-            }
-        })
-    })
-}
+zero.addEventListener("click", (e) => {
+  if (displayArr.length >= 1) {
+    if (resultButton.classList.length == 1) {
+      assignFirst(zero.textContent);
+    } else {
+      assignSecond(zero.textContent);
+    }
+  }
+});
 
-function buttonZeroToDisplay() {
-    zero.addEventListener("click", (e) => {
-        if (displayArr.length >= 1) {
-            displayArr.push(zero.textContent)
-            display.textContent = displayArr.join("")
-        }
-    })
-}
+resetButton.addEventListener("click", () => {
+  reset();
+});
 
-function reset() {
-    resetButton.addEventListener('click', () => {
-        displayArr = []
-        display.textContent = "0"
-    })
-}
+addButton.addEventListener("click", () => {
+  if (resultButton.classList.contains("add")) {
+    addTrigger();
+  }
+  displayArr = [];
+  digits.forEach((item) => {
+    item.classList.add("operation");
+  });
+  zero.classList.add("operation");
+  resultButton.classList.add("add");
+});
+
+resultButton.addEventListener("click", () => {
+  if (resultButton.classList.contains("add")) {
+    addTrigger();
+    resultButton.classList.remove("add");
+  }
+  digits.forEach((item) => {
+    item.classList.remove("operation");
+  });
+});
 
 function addTrigger() {
-    addButton.addEventListener('click', () => {
-        if (resultButton.classList.contains("add")) {
-            resultVal = add(firstVal, secondVal)
-            display.textContent = resultVal
-            firstVal = resultVal
-        }
-        displayArr = []
-        digits.forEach((item) => {
-            item.classList.add("operation")
-        })
-        zero.classList.add("operation")
-        resultButton.classList.add("add")
-    })
-}
-
-function triggerResult() {
-    resultButton.addEventListener('click', () => {
-        resultVal = add(firstVal, secondVal)
-        display.textContent = resultVal
-        digits.forEach((item) => {
-            item.classList.remove("operation")
-        })
-        resultButton.classList.remove("add")
-        firstVal = resultVal
-    })
+  resultVal = add(firstVal, secondVal);
+  display.textContent = resultVal;
+  firstVal = resultVal;
 }
 
 function add(num1, num2) {
-    return num1 + num2
+  return num1 + num2;
+}
+
+function assignFirst(num) {
+  displayArr.push(num);
+  firstVal = parseInt(displayArr.join(""));
+  secondVal = firstVal;
+  display.textContent = firstVal;
+}
+
+function assignSecond(num) {
+  displayArr.push(num);
+  secondVal = parseInt(displayArr.join(""));
+  display.textContent = secondVal;
+}
+
+function reset() {
+  displayArr = [];
+  display.textContent = "0";
+  firstVal = null;
+  secondVal = null;
+  resultVal = null;
 }
