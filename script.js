4b69ec0e64751de64e196873d7213170881fa05a
bima-taruna <bimataruna21@gmail.com>
2024-03-21 14:07:23 +0700

    implement add function in script js

--- script.js
+++ script.js
@@ -2,18 +2,34 @@ const digits = document.querySelectorAll(".digit")
 const display = document.querySelector(".display")
 const zero = document.querySelector(".zero")
 const resetButton = document.querySelector('.clear')
-let displayArr = []
+const addButton = document.querySelector(".add")
+const resultButton = document.querySelector(".result")
+let firstVal = null;
+let secondVal = null;
+let resultVal = null;
+let displayArr = [];
 
 buttonToDisplay()
 buttonZeroToDisplay()
+addTrigger()
 reset()
+triggerResult()
 
 function buttonToDisplay() {
     digits.forEach((item) => {
         item.addEventListener("click", () => {
             if (displayArr.length < 9) {
-                displayArr.push(item.textContent)
-                display.textContent = displayArr.join("")
+                if (!item.classList.contains("operation")) {
+                    displayArr.push(item.textContent)
+                    firstVal = parseInt(displayArr.join(""))
+                    secondVal = firstVal
+                    display.textContent = firstVal
+                } else {
+                    displayArr = []
+                    displayArr.push(item.textContent)
+                    secondVal = parseInt(displayArr.join(""))
+                    display.textContent = secondVal
+                }
             }
         })
     })
@@ -33,4 +49,36 @@ function reset() {
         displayArr = []
         display.textContent = "0"
     })
+}
+
+function addTrigger() {
+    addButton.addEventListener('click', () => {
+        if (resultButton.classList.contains("add")) {
+            resultVal = add(firstVal, secondVal)
+            display.textContent = resultVal
+            firstVal = resultVal
+        }
+        displayArr = []
+        digits.forEach((item) => {
+            item.classList.add("operation")
+        })
+        zero.classList.add("operation")
+        resultButton.classList.add("add")
+    })
+}
+
+function triggerResult() {
+    resultButton.addEventListener('click', () => {
+        resultVal = add(firstVal, secondVal)
+        display.textContent = resultVal
+        digits.forEach((item) => {
+            item.classList.remove("operation")
+        })
+        resultButton.classList.remove("add")
+        firstVal = resultVal
+    })
+}
+
+function add(num1, num2) {
+    return num1 + num2
 }
